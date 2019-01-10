c958a7c70f282f290a7774535a0af7c6e37806ef
Austin Beaufort <tinyhatcoding@gmail.com>
2019-01-10 10:59:54 -0500

    Version 2: Text Based Alarm Clock!

--- extras.js
+++ extras.js
@@ -1,15 +1,30 @@
-// VERSION 1 ----------------------------------------------------------------------
+// VERSION 2 ----------------------------------------------------------------------
 
 
 // CREATED A DEBUGGER FUNCTION TO USE IN CODE
 
-function logTenNumbers() {
-    for (let i = 0; i < 10; i++) {
-        console.log(i);
-    }
-}
+// function logTenNumbers() {
+//     for (let i = 0; i < 10; i++) {
+//         console.log(i);
+//     }
+// }
 
-function runWithDebugger(ourFunction) {
-    debugger;
-    ourFunction();
-}
+// function runWithDebugger(ourFunction) {
+//     debugger;
+//     ourFunction();
+// }
+
+
+
+
+
+
+// TEXT BASED ALARM CLOCK!
+
+// function myTimer() {
+
+//     setTimeout(function() {
+//         console.log('Wake up Gordon!');
+//     }, 5000)
+    
+// }
